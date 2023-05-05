5a02b4034e57b9d675c6c2e3c209a2e84499670d
= <=>
2023-05-05 23:42:21 +0300

    working on note create

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./styles/NotesPage.module.css";
+import React, { useEffect, useState } from "react";
+import { Note as NoteModel } from "./models/note";
+import Note from "./components/Note";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import * as NotesApi from "./network/notes_api";
+import AddNoteDialog from "./components/AddNoteDialog";
 
 function App() {
+  const [notes, setNotes] = useState<NoteModel[]>([]);
+
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const notes = await NotesApi.fetchNotes();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button onClick={() => setShowAddNoteDialog(true)}>Add new note</Button>
+
+      <Row xs={1} md={2} xl={3} className="g-4">
+        {notes.map((note) => (
+          <Col key={note._id}>
+            <Note note={note} className={styles.note} />
+          </Col>
+        ))}
+      </Row>
+      {showAddNoteDialog && (
+        <AddNoteDialog
+          onNoteSaved={() => {}}
+          onDismiss={() => setShowAddNoteDialog(false)}
+        />
+      )}
+    </Container>
   );
 }
 
---
+++ frontend/src/components/AddNoteDialog.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { Note } from "../models/note";
+import { useForm } from "react-hook-form";
+import { NoteInput } from "../network/notes_api";
+import * as NoteApi from "../network/notes_api";
+
+interface AddNoteDialogProps {
+  onDismiss: () => void;
+  onNoteSaved: (note: Note) => void;
+}
+
+const AddNoteDialog = ({ onDismiss, onNoteSaved }: AddNoteDialogProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<NoteInput>();
+
+  async function onSubmitHandler(input: NoteInput) {
+    console.log("register");
+    try {
+      const noteResponse = await NoteApi.createNote(input);
+      onNoteSaved(noteResponse);
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+  return (
+    <Modal show onHide={onDismiss}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add Note</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form id="addNoteForm" onSubmit={(e) => handleSubmit(onSubmitHandler)}>
+          <Form.Group className="mb-3">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Title"
+              isInvalid={!!errors.title}
+              {...register("title", { required: "Required" })}
+            />
+            <Form.Control.Feedback type="invalid">
+              Something went wrong
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Text</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Text"
+              {...register("text")}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button type="submit" form="addNoteForm" disabled={isSubmitting}>
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default AddNoteDialog;
