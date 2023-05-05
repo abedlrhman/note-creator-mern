029264f72b75cb07d1366a31fd54874a7793aa93
= <=>
2023-05-05 23:53:22 +0300

    finish note create

--- frontend/src/components/AddNoteDialog.tsx
+++ frontend/src/components/AddNoteDialog.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Form, Modal } from "react-bootstrap";
+import { Button, Form, FormGroup, Modal } from "react-bootstrap";
 import { Note } from "../models/note";
 import { useForm } from "react-hook-form";
 import { NoteInput } from "../network/notes_api";
@@ -17,8 +17,7 @@ const AddNoteDialog = ({ onDismiss, onNoteSaved }: AddNoteDialogProps) => {
     formState: { errors, isSubmitting },
   } = useForm<NoteInput>();
 
-  async function onSubmitHandler(input: NoteInput) {
-    console.log("register");
+  async function onSubmit(input: NoteInput) {
     try {
       const noteResponse = await NoteApi.createNote(input);
       onNoteSaved(noteResponse);
@@ -34,29 +33,31 @@ const AddNoteDialog = ({ onDismiss, onNoteSaved }: AddNoteDialogProps) => {
       </Modal.Header>
 
       <Modal.Body>
-        <Form id="addNoteForm" onSubmit={(e) => handleSubmit(onSubmitHandler)}>
-          <Form.Group className="mb-3">
-            <Form.Label>Title</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Title"
-              isInvalid={!!errors.title}
-              {...register("title", { required: "Required" })}
-            />
-            <Form.Control.Feedback type="invalid">
-              Something went wrong
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Text</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={5}
-              placeholder="Text"
-              {...register("text")}
-            />
-          </Form.Group>
-        </Form>
+        <form id="addNoteForm" onSubmit={handleSubmit(onSubmit)}>
+          <FormGroup role="form">
+            <Form.Group className="mb-3">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Title"
+                isInvalid={!!errors.title}
+                {...register("title", { required: "Required" })}
+              />
+              <Form.Control.Feedback type="invalid">
+                Something went wrong
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Text</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={5}
+                placeholder="Text"
+                {...register("text")}
+              />
+            </Form.Group>
+          </FormGroup>
+        </form>
       </Modal.Body>
       <Modal.Footer>
         <Button type="submit" form="addNoteForm" disabled={isSubmitting}>
